6df1cf655375dc84f526c110d375ccdc8089638f
Sang Dang <sangdase170287@fpt.edu.vn>
2023-01-16 23:53:20 +0700

    main javascript coding
    today i did a lot of annoying debugging for this nonblank. seems like nested input fields are a pain to deal with. i need a better way of dealing with this other than just brute forcing the solution by dealing with each different ID seperately

---
+++ js/script.js
@@ -0,0 +1,71 @@
+function updateValueBasic(start, des) {
+    if (start.value === "") {
+        des.innerHTML = start.getAttribute('data-empty');
+        return;
+    }
+    des.innerHTML = start.value;
+}
+
+function updateValueCardNumber(start, des) {
+    if (start.value === "") {
+        des.innerHTML = start.getAttribute('data-empty');
+        return;
+    }
+    var result = "";
+    var source = start.value.replace(/\s/g, '');
+    for (var i = 0; i < source.length; i++) {
+        if (i % 4 == 0 && i != 0) {
+            result += " ";
+        }
+        result += source[i];
+    }
+    des.innerHTML = result;
+    start.value = result;
+}
+
+function onlyNumber(start) {
+    start.value = start.value.replace(/[^0-9]/g, "");
+}
+
+function getErrorTag(item) {
+    do {
+        if (item.classList.contains("error")) {
+            return item;
+        } else {
+            item = item.nextElementSibling;
+        }
+    } while (item != null);
+    return null;
+}
+
+const nonblank = document.querySelectorAll(".nonblank");
+nonblank.forEach(item => {
+    item.addEventListener('blur', () => {
+        if (item.value === "") {
+            item.classList.add("active");
+            getErrorTag(item).innerHTML = "Can't be blank";
+            getErrorTag(item).classList.add("active");
+
+        } else {
+            item.classList.remove("active");
+            getErrorTag(item).classList.remove("active");
+        }
+    })
+})
+
+const onlynumbers = document.querySelectorAll(".onlynumbers");
+const reg = new RegExp(/^[0-9 ]+$/);
+onlynumbers.forEach(item => {
+    item.addEventListener('blur', () => {
+        if (item.value != "") {
+            if (!reg.test(item.value)) {
+                item.classList.add("active");
+                getErrorTag(item).innerHTML = "Wrong format. Numbers only.";
+                getErrorTag(item).classList.add("active");
+            } else {
+                item.classList.remove("active");
+                getErrorTag(item).classList.remove("active");
+            }
+        }
+    })
+})
